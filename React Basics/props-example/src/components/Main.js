edde07a304bb70ba055b38033979c8a083d98b31
Evan Varan <evan.varan@gmail.com>
2025-07-01 13:17:45 -0500

    Props example

---
+++ React Basics/props-example/src/components/Main.js
@@ -0,0 +1,4 @@
+function Main(props){
+    return <h2>{props.greet}, From Main</h2>
+}
+export default Main;
---
+++ React Basics/props-example/src/components/Sidebar.js
@@ -0,0 +1,4 @@
+function Sidebar(props){
+    return <h2>{props.greet}, From Sidebar</h2>
+}
+export default Sidebar;
