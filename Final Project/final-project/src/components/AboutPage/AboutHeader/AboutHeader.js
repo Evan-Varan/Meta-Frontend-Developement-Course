8651311dd9886f5e009e7e9ac108f11cc914f3ae
Evan Varan <evan.varan@gmail.com>
2025-10-05 15:51:48 -0500

    Minor Structural Fixes

--- Final Project/final-project/src/components/AboutPage/AboutHeader/AboutHeader.js
+++ Final Project/final-project/src/components/AboutPage/AboutHeader/AboutHeader.js
@@ -7,15 +7,17 @@ import { FaArrowRightLong } from "react-icons/fa6";
 export default function AboutHeader(){
     return(
         <div className="about-header">
-            <div className="text-chunk">
-                <h2>About Us</h2>
-                <h3>About Little Lemon</h3>
-                <div className="divider"></div>
-            </div>
-            <div className="text-chunk">
-                <p className="description-tag">Discover the heart of Mediterranean-inspired cuisine at Little Lemon. Blending tradition with fresh, vibrant flavors, our specials celebrate both comfort and creativity. Whether you’re joining us for a reserved evening or savoring our dishes at home, every bite brings the warmth of authentic hospitality.</p>
-                <p className="description-tag">Discover the heart of Mediterranean-inspired cuisine at Little Lemon. Blending tradition with fresh, vibrant flavors, our specials celebrate both comfort and creativity. Whether you’re joining us for a reserved evening or savoring our dishes at home, every bite brings the warmth of authentic hospitality.</p>
-                <TertiaryButton text= "View All Locations" Icon = {FaArrowRightLong}/>
+            <div className="non-img-header">
+                <div className="text-chunk">
+                    <h2>About Us</h2>
+                    <h3>About Little Lemon</h3>
+                    <div className="divider"></div>
+                </div>
+                <div className="text-chunk">
+                    <p className="description-tag">Discover the heart of Mediterranean-inspired cuisine at Little Lemon. Blending tradition with fresh, vibrant flavors, our specials celebrate both comfort and creativity. Whether you’re joining us for a reserved evening or savoring our dishes at home, every bite brings the warmth of authentic hospitality.</p>
+                    <p className="description-tag">Discover the heart of Mediterranean-inspired cuisine at Little Lemon. Blending tradition with fresh, vibrant flavors, our specials celebrate both comfort and creativity. Whether you’re joining us for a reserved evening or savoring our dishes at home, every bite brings the warmth of authentic hospitality.</p>
+                    <TertiaryButton text= "View All Locations" Icon = {FaArrowRightLong}/>
+                </div>
             </div>
             <img src={LLLogo} className="lllogo"/>
         </div>
--- Final Project/final-project/src/components/Footer/Footer.js
+++ Final Project/final-project/src/components/Footer/Footer.js
@@ -2,25 +2,34 @@ import "./Footer.css"
 
 export default function Footer(){
     return(
-        <div className="footer-col">
+        <footer className="footer-col">
             <h1>Little Lemon</h1>
             <div className="footer-row">
                 <div className="info-col">
                     <div className="divider"></div>
                     <h2>INFORMATION</h2>
                     <div className="info-text-col">
-                        <p>text1</p>
-                        <p>text2</p>
-                        <p>text3</p>
+                        <p>Little Lemon Restaurant</p>
+                        <p>1009 Vulture Rd, Chicago, IL</p>
+                        <p>info@littlelemon.com</p>
                     </div>
                 </div>
                 <div className="info-col">
                     <div className="divider"></div>
                     <h2>SITEMAP</h2>
                     <div className="info-text-col">
-                        <p>text1</p>
-                        <p>text2</p>
-                        <p>text3</p>
+                        <p>HOME</p>
+                        <p>SPECIALS</p>
+                        <p>ABOUT</p>
+                    </div>
+                </div>
+                <div className="info-col">
+                    <div className="divider"></div>
+                    <h2>SITEMAP</h2>
+                    <div className="info-text-col">
+                        <p>TESTIMONIALS</p>
+                        <p>EXPERIENCE</p>
+                        <p>GALLERY</p>
                     </div>
                 </div>
                 
@@ -28,12 +37,12 @@ export default function Footer(){
                     <div className="divider"></div>
                     <h2>FOLLOW US</h2>
                     <div className="info-text-col">
-                        <p>text1</p>
-                        <p>text2</p>
-                        <p>text3</p>
+                        <p>INSTAGRAM</p>
+                        <p>FACEBOOK</p>
+                        <p>TIKTOK</p>
                     </div>
                 </div>
             </div>
-        </div>
+        </footer>
     )
 }
