5320f107e785374fd5adfb7499e4f70e565afd53
Evan Varan <evan.varan@gmail.com>
2025-10-08 14:49:51 -0500

    Internal validation adding - removed standard  html validation

--- Final Project/final-project/src/components/BookingPage/ContactInfo/ContactInfo.js
+++ Final Project/final-project/src/components/BookingPage/ContactInfo/ContactInfo.js
@@ -1,8 +1,9 @@
 import "./ContactInfo.css"
 import TertiaryButton from "../../Buttons/TertiaryButton/TertiaryButton"
+import { BiError } from "react-icons/bi";
+
 
 /*TODO:
-    Our own validation instead of builtin validation message
     Regex for email and phone
 */
 
@@ -22,29 +23,48 @@ export default function ContactInfo({setStep}){
 
 
     return(
-        <form className="contact-col" onSubmit={handleFormSubmission}>
+        /* noValidate allows us to just to validation through the onSubmit without the popups */
+        <form className="contact-col" noValidate onSubmit={handleFormSubmission}>
             <h2>Enter your details</h2>
-            <div className="contact-row">
-                <div className="input-group">
-                    <label>First Name</label>
-                    <input type = "text" placeholder ="First Name" id="first-name" minLength ="2" name="first-name" required autoComplete = "given-name" className="contact-input"></input>
+            <div className="contact-section">
+                <div className="contact-row">
+                    <div className="input-group">
+                        <label>First Name</label>
+                        <input type = "text" placeholder ="First Name" id="first-name" minLength ="2" name="first-name" required autoComplete = "given-name" className="contact-input"></input>
+                        <div className="error-message">
+                            <BiError color = "#e74c3c"/>
+                            <p>Please enter your first name</p>
+                        </div>
+                    </div>
+                    <div className="input-group">
+                        <label>Last Name</label>
+                        <input type = "text" placeholder ="Last Name" id="last-name" minLength ="2" name="last-name" required autoComplete = "family-name" className="contact-input"></input>
+                        <div className="error-message">
+                            <BiError color = "#e74c3c"/>
+                            <p>Please enter your last name</p>
+                        </div>
+                    </div>
                 </div>
                 <div className="input-group">
-                    <label>Last Name</label>
-                    <input type = "text" placeholder ="Last Name" id="last-name" minLength ="2" name="last-name" required autoComplete = "family-name" className="contact-input"></input>
+                    <label>Email</label>
+                    <input type = "email" placeholder ="Email" id="email" name="email" required autoComplete = "email" className="contact-input"></input>
+                    <div className="error-message">
+                            <BiError color = "#e74c3c"/>
+                            <p>Please enter your email</p>
+                    </div>
                 </div>
-            </div>
-            <div className="input-group">
-                <label>Email</label>
-                <input type = "email" placeholder ="Email" id="email" name="email" required autoComplete = "email" className="contact-input"></input>
-            </div>
-            <div className="input-group">
-                <label>Company - Optional</label>
-                <input type = "text" placeholder ="Company - Optional" id="company" minLength ="2" name="company" autoComplete = "organization"className="contact-input"></input>
-            </div>
-            <div className="input-group">
-                <label>Phone</label>
-                <input type = "tel" placeholder ="Phone"  id="phone" minLength ="10" name="phone" required autoComplete = "tel" pattern="[0-9]{10}" className="contact-input"></input>
+                <div className="input-group">
+                    <label>Company - Optional</label>
+                    <input type = "text" placeholder ="Company - Optional" id="company" minLength ="2" name="company" autoComplete = "organization"className="contact-input"></input>
+                </div>
+                <div className="input-group">
+                    <label>Phone</label>
+                    <input type = "tel" placeholder ="Phone"  id="phone" minLength ="10" name="phone" required autoComplete = "tel" pattern="[0-9]{10}" className="contact-input"></input>
+                    <div className="error-message">
+                        <BiError color = "#e74c3c"/>
+                        <p>Please enter your phone number</p>
+                    </div>
+                </div>
             </div>
             <TertiaryButton
             type="submit"
